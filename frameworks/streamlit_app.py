ceffd18c1c126862a72e10b3967221ea1e07ea07
brendenmcfarlane <1mcfarlaneb1@gmail.com>
2026-06-24 01:40:35 -0400

    streamlit update

--- frameworks/streamlit_app.py
+++ frameworks/streamlit_app.py
@@ -1,7 +1,11 @@
 # python3 -m streamlit run frameworks/script.py
 import streamlit as st
 import pandas as pd
+from application.database_interface import JSONDB
 
+jsondb = JSONDB()
+transcript = jsondb.read_in("tests/clean_trace/sample_trace.json")
+st.write(transcript)
 st.write("Here's our first attempt at using data to create a table:")
 st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
