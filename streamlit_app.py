e5216bc27dd0b5d0cc663ad9a2678943d421c80a
brendenmcfarlane <1mcfarlaneb1@gmail.com>
2026-06-24 14:55:29 -0400

    streamlit update

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,27 +6,27 @@ from test_imp import TEST_VAR
 from application.workflow_node import WorkFlowNode
 
 st.write(TEST_VAR)
-class WorkFlowNode():
-    def __init__(self, key:str):
-        self._id = key
-        self._children = []
-        self._transcript = []
-        self._adjacencies = []
+# class WorkFlowNode():
+#     def __init__(self, key:str):
+#         self._id = key
+#         self._children = []
+#         self._transcript = []
+#         self._adjacencies = []
 
-    def get_workflow(self):
-        return self._adjacencies
+#     def get_workflow(self):
+#         return self._adjacencies
 
-    def add_child(self, child):
-        self._children.append(child)
+#     def add_child(self, child):
+#         self._children.append(child)
 
-    def get_children(self):
-        return self._children
+#     def get_children(self):
+#         return self._children
     
-    def add_adjacency(self, adj):
-        self._adjacencies.append(adj)
+#     def add_adjacency(self, adj):
+#         self._adjacencies.append(adj)
 
-    def set_transcript(self, transcript:list[dict]):
-        self._transcript = transcript
+#     def set_transcript(self, transcript:list[dict]):
+#         self._transcript = transcript
 
 clean_trace = [
     {
