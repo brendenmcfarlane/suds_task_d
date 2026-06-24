385dce2be3fe45b885f36706d79cd462ff4836fc
brendenmcfarlane <1mcfarlaneb1@gmail.com>
2026-06-24 01:33:20 -0400

    streamlit update

--- frameworks/streamlit_app.py
+++ frameworks/streamlit_app.py
@@ -7,5 +7,5 @@ st.write(pd.DataFrame({
     'first column': [1, 2, 3, 4],
     'second column': [10, 20, 30, 40]
 }))
-x = st.slider('x')  
-st.write(x, 'squared is', x * x)
+y = st.slider('y')  
+st.write(y, 'squared is', y * y)
