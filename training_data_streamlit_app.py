7c3e26d49b8ea350b3113f78a8a8abd792c3c89d
brendenmcfarlane <1mcfarlaneb1@gmail.com>
2026-07-13 14:43:47 -0400

    updateing streamlit view

--- training_data_streamlit_app.py
+++ training_data_streamlit_app.py
@@ -72,9 +72,9 @@ right_key = col2.radio(
 if isinstance(left_key, int):
     col1.subheader("Transcript")
     t = mas_state_list[left_key].get_partial_transcript()
-    col1.write(t)
+    col1.json(t)
 
 if isinstance(right_key, int):
     col2.subheader("Transcript")
     t = mas_state_list[right_key].get_partial_transcript()
-    col2.write(t)
+    col2.json(t)
